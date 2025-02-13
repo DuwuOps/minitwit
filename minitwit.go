c6a4502ae34455df6e10ca1767a98678ba8bbb82
David Martin Sørensen <75354353+SirSorensen@users.noreply.github.com>
2025-02-13 20:22:41 +0100

    Feat : Added method for converting sql.rows to list of maps. Took heavy inspiration from https://kylewbanks.com/blog/query-result-to-map-in-golang
    Co-authored-by: Gustav <guch@itu.dk>
    Co-authored-by: Dea <deathim@icloud.com>
    Co-authored-by: Nikoline <nikoline.burman@gmail.com>

--- minitwit.go
+++ minitwit.go
@@ -94,6 +94,39 @@ func queryDB(db *sql.DB, query string, singleResult bool, args ...any) (*sql.Row
 	return rows, nil
 }
 
+func rowsToMapList(rows *sql.Rows) ([]map[string]interface{}, error) {
+    var result []map[string]interface{}
+    cols, _ := rows.Columns()
+
+    for rows.Next() {
+        // Create a slice of interface{}'s to represent each column,
+        // and a second slice to contain pointers to each item in the columns slice.
+        columns := make([]interface{}, len(cols))
+        columnPointers := make([]interface{}, len(cols))
+        for i, _ := range columns {
+            columnPointers[i] = &columns[i]
+        }
+        
+        // Scan the result into the column pointers...
+        if err := rows.Scan(columnPointers...); err != nil {
+            return nil, err
+        }
+
+        // Create our map, and retrieve the value for each column from the pointers slice,
+        // storing it in the map with the name of the column as the key.
+        m := make(map[string]interface{})
+        for i, colName := range cols {
+            val := columnPointers[i].(*interface{})
+            m[colName] = *val
+        }
+        
+        // Outputs: map[columnName:value columnName2:value2 columnName3:value3 ...] 
+        fmt.Print(m)
+        result = append(result, m)
+    }
+
+    return result, nil
+}
 
 // ==========================
 // Start: Routes
