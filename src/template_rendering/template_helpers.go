c554a97f4110ff6a385765e2ce344a4ab0a64098
Nikoline Burman <nikoline.burman@gmail.com>
2025-03-07 17:28:20 +0100

    Fix : Set time to local time.

--- src/template_rendering/template_helpers.go
+++ src/template_rendering/template_helpers.go
@@ -19,6 +19,6 @@ func gravatarUrl(email string, size int) string {
 }
 
 func formatDatetime(timestamp int64) string {
-	time := time.Unix(timestamp, 0).UTC()
+	time := time.Unix(timestamp, 0).UTC().Local()
 	return time.Format("2006-01-02 @ 15:04")
 }
