f4b8ee70748851a2091223498c47a411a017f715
Simone Haukjær Jakobsen <shj@guide-it.dk>
2025-03-13 16:44:48 +0100

    Use the repository in message handler + refactorings to follow DRY

--- src/datalayer/irepository.go
+++ src/datalayer/irepository.go
@@ -8,6 +8,7 @@ type IRepository[T any] interface {
     Create(ctx context.Context, entity *T) error
     GetByID(ctx context.Context, id int) (*T, error)
     GetAll(ctx context.Context) ([]T, error)
+    GetFiltered(ctx context.Context, conditions map[string]any, limit int) ([]T, error)
     Update(ctx context.Context, entity *T) error
     Remove(ctx context.Context, id int) error
 }
