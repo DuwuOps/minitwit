887fd45258f5e6fde077cedd760fd7e26d6ce71c
Babette Bækgaard (BabetteB) <babetteb@outlook.dk>
2025-04-09 15:51:01 +0200

    feat: add ParseFollowerBuckets to parse follower/followee intervals for grafana

---
+++ src/handlers/helpers/parsers.go
@@ -0,0 +1,37 @@
+package helpers
+
+import (
+    "fmt"
+    "os"
+    "strconv"
+    "strings"
+)
+
+func ParseFollowerBuckets(envVarName string) ([][2]uint32, error) {
+    val := os.Getenv(envVarName)
+    if val == "" {
+        return nil, fmt.Errorf("environment variable %s is not set", envVarName)
+    }
+
+    pairs := strings.Split(val, ",")
+    buckets := make([][2]uint32, 0, len(pairs))
+
+    for _, pair := range pairs {
+        rng := strings.Split(pair, "-")
+        if len(rng) != 2 {
+            return nil, fmt.Errorf("invalid format in %s, expecting something like '100-200'", pair)
+        }
+        low, err := strconv.ParseUint(rng[0], 10, 32)
+        if err != nil {
+            return nil, fmt.Errorf("failed to parse lower bound %s: %v", rng[0], err)
+        }
+        high, err := strconv.ParseUint(rng[1], 10, 32)
+        if err != nil {
+            return nil, fmt.Errorf("failed to parse upper bound %s: %v", rng[1], err)
+        }
+
+        buckets = append(buckets, [2]uint32{uint32(low), uint32(high)})
+    }
+
+    return buckets, nil
+}
