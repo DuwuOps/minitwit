31de9d72a3917cc1ba2f4325a0ef3ff63c8f7949
Nikoline Burman <nikoline.burman@gmail.com>
2025-05-02 22:16:39 +0200

    Fix : Initialize latest_processed table with value 0

--- src/queries/schema.latest_processed.sql
+++ src/queries/schema.latest_processed.sql
@@ -2,3 +2,5 @@ CREATE TABLE IF NOT EXISTS latest_processed (
   latest_processed_id INTEGER PRIMARY KEY,
   date_updated INTEGER
 );
+
+INSERT INTO latest_processed (latest_processed_id, date_updated) VALUES (0, 0000000000);
