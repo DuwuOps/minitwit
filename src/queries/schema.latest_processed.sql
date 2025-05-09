f1a7719659cd4eadf55837bf725dafc714f34256
David Martin Sørensen <daso@itu.dk>
2025-05-09 09:13:47 +0200

    Fix : latest_processed id 0 exists

--- src/queries/schema.latest_processed.sql
+++ src/queries/schema.latest_processed.sql
@@ -2,5 +2,3 @@ CREATE TABLE IF NOT EXISTS latest_processed (
   latest_processed_id INTEGER PRIMARY KEY,
   date_updated INTEGER
 );
-
-INSERT INTO latest_processed (latest_processed_id, date_updated) VALUES (0, 0000000000);
