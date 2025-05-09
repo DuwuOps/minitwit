ec1aeb4e840f17fd2167d46f6feefbdc35a3b854
David Martin Sørensen <daso@itu.dk>
2025-05-09 09:25:24 +0200

    Fix : latest_processed SQL on conflict do nothing

--- src/queries/schema.latest_processed.sql
+++ src/queries/schema.latest_processed.sql
@@ -3,8 +3,4 @@ CREATE TABLE IF NOT EXISTS latest_processed (
   date_updated INTEGER
 );
 
-IF EXISTS (SELECT * FROM latest_processed WHERE latest_processed_id = 0)
-UPDATE latest_processed SET date_updated = 0000000000 WHERE latest_processed_id = 0
-ELSE
-INSERT INTO latest_processed (latest_processed_id, date_updated) VALUES (0, 0000000000)
-END IF
+INSERT INTO latest_processed (latest_processed_id, date_updated) VALUES (0, 0000000000) ON CONFLICT DO NOTHING;
