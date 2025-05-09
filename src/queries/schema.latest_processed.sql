4d188eb701456783745721bce65aada1bd951fe4
David Martin Sørensen <daso@itu.dk>
2025-05-09 09:19:10 +0200

    Fix : latest_processed inserts at startup

--- src/queries/schema.latest_processed.sql
+++ src/queries/schema.latest_processed.sql
@@ -2,3 +2,9 @@ CREATE TABLE IF NOT EXISTS latest_processed (
   latest_processed_id INTEGER PRIMARY KEY,
   date_updated INTEGER
 );
+
+IF EXISTS (SELECT * FROM latest_processed WHERE latest_processed_id = 0)
+UPDATE latest_processed SET date_updated = 0000000000 WHERE latest_processed_id = 0
+ELSE
+INSERT INTO latest_processed (latest_processed_id, date_updated) VALUES (0, 0000000000)
+END IF
