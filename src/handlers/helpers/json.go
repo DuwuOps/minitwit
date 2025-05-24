ce62fc438c99dc756894bb405b4e43a2f24a16fb
David Martin Sørensen <daso@itu.dk>
2025-05-24 13:53:55 +0200

    Refactor : json.go uses "log/slog" instead of "log"

--- src/handlers/helpers/json.go
+++ src/handlers/helpers/json.go
@@ -2,7 +2,8 @@ package helpers
 
 import (
 	"encoding/json"
-	"log"
+	"log/slog"
+	"minitwit/src/utils"
 
 	"github.com/labstack/echo/v4"
 )
@@ -12,7 +13,7 @@ func ExtractJson(c echo.Context) (map[string]any, error) {
 	jsonBody := make(map[string]any)
 	err := json.NewDecoder(c.Request().Body).Decode(&jsonBody)
 	if err != nil {
-		log.Printf("json.NewDecoder returned error: %v\n", err)
+		utils.LogError("json.NewDecoder returned an error", err)
 		return nil, err
 	}
 
@@ -22,9 +23,9 @@ func ExtractJson(c echo.Context) (map[string]any, error) {
 func GetStringValue(jsonBody map[string]any, key string) string {
 	result := jsonBody[key]
 	if result == nil {
-		log.Printf("result of %v: nil\n", key)
+		slog.Info("GetStringValue: jsonBody does not contain key", slog.Any("key", key))
 		return ""
 	}
-	log.Printf("result.(string) of %v: %v\n", key, result.(string))
+	slog.Info("GetStringValue: succesful", slog.Any("key", key), slog.Any("result", result.(string)))
 	return result.(string)
 }
