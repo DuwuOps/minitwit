9bd261759763e7b573494e91b88f7ed519f875c7
David Martin Sørensen <daso@itu.dk>
2025-05-24 13:56:39 +0200

    Refactor : handlers/user.go.go uses "log/slog" instead of "log"

--- src/handlers/user.go
+++ src/handlers/user.go
@@ -2,40 +2,41 @@ package handlers
 
 import (
 	"fmt"
-	"log"
+	"log/slog"
 	"net/http"
 
 	"minitwit/src/handlers/helpers"
 	"minitwit/src/handlers/repo_wrappers"
+	"minitwit/src/utils"
 
 	"github.com/labstack/echo/v4"
 )
 
 func Follow(c echo.Context) error {
 	username := c.Param("username")
-	log.Printf("🎺 User entered Follow via route \"/fllws/:username\" as \"/%v\"\n", username)
+	utils.LogRouteStart(c, "Follow", "/fllws/:username")
 
 	err := repo_wrappers.UpdateLatest(c)
 	if err != nil {
-		log.Printf("repo_wrappers.UpdateLatest returned error: %v\n", err)
+		utils.LogError("repo_wrappers.UpdateLatest returned an error", err)
 		return err
 	}
 
 	err = helpers.NotReqFromSimulator(c)
 	if err != nil {
-		log.Printf("notReqFromSimulator returned error: %v\n", err)
+		utils.LogError("notReqFromSimulator returned an error", err)
 		return err
 	}
 
 	user, err := repo_wrappers.GetUserByUsername(c.Request().Context(), username)
 	if err != nil {
-		log.Printf("getUserId returned error: %v\n", err)
+		utils.LogError("getUserId returned an error", err)
 		return err
 	}
 
 	payload, err := helpers.ExtractJson(c)
 	if err != nil {
-		log.Printf("Follow: ExtractJson returned error: %v\n", err)
+		utils.LogErrorContext(c.Request().Context(), "Follow: ExtractJson returned an error", err)
 	}
 	
 
@@ -51,10 +52,10 @@ func Follow(c echo.Context) error {
 	}
 
 	if c.Request().Method == http.MethodPost && followsUsername != "" {
-		log.Printf("User \"%v\" has requested to follow \"%v\"\n", username, followsUsername)
+		slog.InfoContext(c.Request().Context(), "User has requested to follow", slog.Any("username", username), slog.Any("followsUsername", followsUsername))
 		follow, err := repo_wrappers.GetUserByUsername(c.Request().Context(), followsUsername)
 		if err != nil {
-			log.Printf("getUserIdreturned error: %v\n", err)
+			utils.LogErrorContext(c.Request().Context(), "getUserId returned an error", err)
 			return err
 		}
 
@@ -63,10 +64,10 @@ func Follow(c echo.Context) error {
 		return c.JSON(http.StatusNoContent, nil)
 
 	} else if c.Request().Method == http.MethodPost && unfollowsUsername != "" {
-		log.Printf("User \"%v\" has requested to unfollow \"%v\"\n", username, unfollowsUsername)
+		slog.InfoContext(c.Request().Context(), "User has requested to unfollow", slog.Any("username", username), slog.Any("followsUsername", unfollowsUsername))
 		unfollow, err := repo_wrappers.GetUserByUsername(c.Request().Context(), unfollowsUsername)
 		if err != nil {
-			log.Printf("getUserId returned error: %v\n", err)
+			utils.LogError("getUserId returned an error", err)
 			return err
 		}
 
@@ -84,7 +85,7 @@ func Follow(c echo.Context) error {
 		followers, err := repo_wrappers.GetFollowerFiltered(c, conditions, noFollowers)
 	
 		if err != nil {
-			log.Printf("Follow: Error retrieving followers for userID=%d: %v", user.UserID, err)
+			slog.ErrorContext(c.Request().Context(), "Follow: Error retrieving followers for userID", slog.Any("userID", user.UserID), slog.Any("error", err))
 			return err
 		}
 
@@ -99,18 +100,18 @@ func Follow(c echo.Context) error {
 		data := map[string]any{
 			"follows": followList,
 		}
-		log.Printf("data: %v\n", data)
+		slog.InfoContext(c.Request().Context(), "", slog.Any("data", data))
 
 		return c.JSON(http.StatusOK, data)
 	}
 
-	log.Printf("ERROR: \"/fllws/:username\" was entered wrongly!\n")
+	slog.Error("\"/fllws/:username\" was entered wrongly!")
 	return c.JSON(http.StatusBadRequest, nil)
 }
 
 func FollowUser(c echo.Context) error {
 	username := c.Param("username")
-	log.Printf("🎺 User entered FollowUser via route \"/:username/follow\" as \"/%v\"\n", username)
+	utils.LogRouteStart(c, "FollowUser", "/:username/follow")
 
 	loggedIn, _ := helpers.IsUserLoggedIn(c)
 	if !loggedIn {
@@ -119,13 +120,13 @@ func FollowUser(c echo.Context) error {
 
 	user, err := repo_wrappers.GetUserByUsername(c.Request().Context(), username)
 	if err != nil {
-		log.Printf("FollowUser: getUserByUsername returned error: %v\n", err)
+		utils.LogErrorContext(c.Request().Context(), "FollowUser: getUserByUsername returned an error", err)
 		c.String(http.StatusNotFound, "Not found")
 	}
 
 	sessionUserId, err := helpers.GetSessionUserID(c)
 	if err != nil {
-		log.Printf("getSessionUserID returned error: %v\n", err)
+		utils.LogError("getSessionUserID returned an error", err)
 		return err
 	}
 	
@@ -133,7 +134,7 @@ func FollowUser(c echo.Context) error {
 
 	err = helpers.AddFlash(c, fmt.Sprintf("You are now following \"%s\"", username))
 	if err != nil {
-		log.Printf("addFlash returned error: %v\n", err)
+		utils.LogError("addFlash returned an error", err)
 	}
 
 	return c.Redirect(http.StatusFound, fmt.Sprintf("/%s", username))
@@ -141,7 +142,7 @@ func FollowUser(c echo.Context) error {
 
 func UnfollowUser(c echo.Context) error {
 	username := c.Param("username")
-	log.Printf("🎺 User entered UnfollowUser via route \"/:username/unfollow\" as \"/%v\"\n", username)
+	utils.LogRouteStart(c, "UnfollowUser", "/:username/unfollow")
 
 	loggedIn, _ := helpers.IsUserLoggedIn(c)
 	if !loggedIn {
@@ -150,13 +151,13 @@ func UnfollowUser(c echo.Context) error {
 
 	user, err := repo_wrappers.GetUserByUsername(c.Request().Context(), username)
 	if err != nil {
-		log.Printf("row.Scan returned error: %v\n", err)
+		utils.LogError("row.Scan returned an error", err)
 		c.String(http.StatusNotFound, "Not found")
 	}
 
 	sessionUserId, err := helpers.GetSessionUserID(c)
 	if err != nil {
-		log.Printf("getSessionUserID returned error: %v\n", err)
+		utils.LogError("getSessionUserID returned an error", err)
 		return err
 	}
 
@@ -164,7 +165,7 @@ func UnfollowUser(c echo.Context) error {
 
 	err = helpers.AddFlash(c, fmt.Sprintf("You are no longer following \"%s\"", username))
 	if err != nil {
-		log.Printf("addFlash returned error: %v\n", err)
+		utils.LogError("addFlash returned an error", err)
 	}
 
 	return c.Redirect(http.StatusFound, fmt.Sprintf("/%s", username))
