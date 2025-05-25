34a5a4d48006f4ed32a2d0db6d6c09dbdfde1441
Babette Bækgaard (BabetteB) <babetteb@outlook.dk>
2025-05-25 10:05:26 +0200

    fix: set database connection limits (max open- and -idle connections and max connection lifetime)

--- src/handlers/repo_wrappers/repos.go
+++ src/handlers/repo_wrappers/repos.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"minitwit/src/datalayer"
 	"minitwit/src/models"
+	"minitwit/src/utils"
 )
 
 var userRepo *datalayer.Repository[models.User]
@@ -12,6 +13,10 @@ var followerRepo *datalayer.Repository[models.Follower]
 var latestProcessedRepo *datalayer.Repository[models.LatestProcessed]
 
 func InitRepos(db *sql.DB) {
+	db.SetMaxOpenConns(utils.GetEnvInt("DB_MAX_OPEN_CONS", 25))
+	db.SetMaxIdleConns(utils.GetEnvInt("DB_MAX_IDLE_CONS", 10))
+	db.SetConnMaxLifetime(utils.GetEnvDuration("DB_MAX_CONN_LIFETIME", "60m"))
+	
 	userRepo = datalayer.NewRepository[models.User](db, "users")
 	messageRepo = datalayer.NewRepository[models.Message](db, "message")
 	followerRepo = datalayer.NewRepository[models.Follower](db, "follower")
