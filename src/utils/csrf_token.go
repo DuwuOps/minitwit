6a6a349a28c6f28056add52d942ade893adda6a4
Babette Bækgaard (BabetteB) <babetteb@outlook.dk>
2025-05-26 16:17:18 +0200

    chore: go format csrf_token file

--- src/utils/csrf_token.go
+++ src/utils/csrf_token.go
@@ -3,17 +3,17 @@ package utils
 import "github.com/labstack/echo/v4"
 
 func GetCsrfToken(c echo.Context) string {
-    tok, ok := c.Get("csrf").(string)
-    if !ok {
-        return ""
-    }
-    return tok
+	tok, ok := c.Get("csrf").(string)
+	if !ok {
+		return ""
+	}
+	return tok
 }
 
 func MapCSRFToContext(c echo.Context, data map[string]interface{}) map[string]interface{} {
-    if data == nil {
-        data = make(map[string]interface{})
-    }
-    data["CSRFToken"] = GetCsrfToken(c)
-    return data
+	if data == nil {
+		data = make(map[string]interface{})
+	}
+	data["CSRFToken"] = GetCsrfToken(c)
+	return data
 }
