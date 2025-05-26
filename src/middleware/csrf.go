e9b587de085a60ff7ee2017ae41523906a05ed02
Babette Bækgaard (BabetteB) <babetteb@outlook.dk>
2025-05-26 16:17:01 +0200

    chore: go format middleware csrf file

--- src/middleware/csrf.go
+++ src/middleware/csrf.go
@@ -1,16 +1,16 @@
 package middleware
 
 import (
-	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func Csrf() echo.MiddlewareFunc {
-  return middleware.CSRFWithConfig(middleware.CSRFConfig{
-        TokenLookup:	"header:X-CSRF-Token,form:_csrf",
-        CookieName:		"csrf_token",
-        CookiePath:		"/",
-        CookieHTTPOnly:	true,
-        CookieSecure:	false, // enable when serving HTTPS
-    })
+	return middleware.CSRFWithConfig(middleware.CSRFConfig{
+		TokenLookup:    "header:X-CSRF-Token,form:_csrf",
+		CookieName:     "csrf_token",
+		CookiePath:     "/",
+		CookieHTTPOnly: true,
+		CookieSecure:   false, // enable when serving HTTPS
+	})
 }
